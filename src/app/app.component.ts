1b9532c11aa9dbf7298dccb229ebdba5d6849086
Juan Monsalve <juanmonsalveh@gmail.com>
2017-11-06 10:48:24 -0500

    Angular Tutorial 4. Multiple Componentes completed ...at least my own version which keep using app.component.html instead of create different .ts files each one with his own html template written on a string variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import  {Hero} from '../model/hero';
+import  {HEROES} from '../mocks/dataMock';
+
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,7 @@ export class AppComponent {
   selectedHero : Hero;
   heroes = HEROES;
 
+
   onSelect(hero: Hero): void {
     if(this.selectedHero && this.selectedHero.id == hero.id){
       this.selectedHero = null;
@@ -19,20 +23,3 @@ export class AppComponent {
   }
 
 }
-export class Hero {
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
