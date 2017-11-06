e680764e11fef1264c03772fae2f1bcb3233c0ec
Juan Monsalve <juanmonsalveh@gmail.com>
2017-11-05 21:48:36 -0500

    Angular Tutorial 3. Master/Detail completed ...plus setting styles on css´s and hidding hero details when selectedHero is clicked again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,56 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`]
+  styleUrls: ['./app.component.css']
   })
 export class AppComponent {
   title = 'Tour of heros tutorial :v xd';
@@ -60,7 +11,11 @@ export class AppComponent {
   heroes = HEROES;
 
   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    if(this.selectedHero && this.selectedHero.id == hero.id){
+      this.selectedHero = null;
+    }else{
+      this.selectedHero = hero;
+    }
   }
 
 }
