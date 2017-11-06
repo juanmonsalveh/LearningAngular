76480189b8bf1353473628ed32f8e5fbac682907
Juan Monsalve <juanmonsalveh@gmail.com>
2017-11-05 20:32:16 -0500

    tour of heroes basic tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: ['./app.component.css'],
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>    `
+  })
 export class AppComponent {
-  title = 'My first angular app :v xd';
+  title = 'Tour of heros tutorial :v xd';
+  hero : Hero = {
+            id: 1,
+            name:'Windstorm'
+        };
+}
+export class Hero {
+  id: number;
+  name: string;
 }
